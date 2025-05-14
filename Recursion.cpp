883480dd31b0063508cfdff21092975e97fa2bea
RaviK0209 <ravikant18712@gmail.com>
2025-05-14 08:21:40 +0530

    recursion explanation

---
+++ Recursion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ return_type recursion(int n){    //recursion function with return type............
+    if(n==1 like that){       
+        return n;             //base condition..............
+    }
+    
+    return recursion(n-1)        //recursion calls...........
+ }
+int main(){
+    int n;
+    cin>>n;
+    cout<<recursion(n);          //main method calling............
+
+}
