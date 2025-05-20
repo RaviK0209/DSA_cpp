4b558bfe3f3ea46aee85ab5a1cc3a601851b3c4d
RaviK0209 <ravikant18712@gmail.com>
2025-05-20 08:49:26 +0530

    find possible paths and print using backtracking

---
+++ Backtracking.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printPath(vector<vector<int>>& matrix, int i, int j, vector<int>& path) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    // Add current cell to path
+    path.push_back(matrix[i][j]);
+
+    // Base case: if we reach the last cell, print the path
+    if (i == rows - 1 && j == cols - 1) {
+        for (int val : path)
+            cout << val << " ";
+        cout << endl;
+    } else {
+        // Move right
+        if (j + 1 < cols)
+            printPath(matrix, i, j + 1, path);
+        // Move down
+        if (i + 1 < rows)
+            printPath(matrix, i + 1, j, path);
+    }
+
+    // Backtrack: remove current cell before going back
+    path.pop_back();
+}
+
+int main() {
+    int m = 2, n = 3;
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    vector<int> path;
+    printPath(matrix, 0, 0, path);
+
+    return 0;
+}
